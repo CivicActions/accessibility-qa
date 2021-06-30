e633021188cca7a9ed0731ba6f90b1cf799e1ff8
CivicActions <>
2021-06-30 18:45:33 +0000

    See https://github.com/CivicActions/accessibility/commit/065ebdf71d22998f5efd71d8991dc6cc5d008bb5 from refs/pull/528/merge

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,13 +1,16 @@
 
 window.onload = function () {
     const accordionHeaders = document.querySelectorAll('.accordion-heading');
-    // const expand = document.querySelector('.expand-all-accordion .expand');
-    // const close = document.querySelector('.close-all-accordion .close');
-    console.log(accordionHeaders);
+
     Array.prototype.forEach.call(accordionHeaders, accordionHeader => {
         let target = accordionHeader.nextElementSibling;
 
         accordionHeader.onclick = () => {
+            let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
+            accordionHeader.setAttribute('aria-expanded', !expanded);
+            target.hidden = expanded;
+        }
+        accordionHeader.onkeypress = () => {
             let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
             accordionHeader.setAttribute('aria-expanded', !expanded);
             target.hidden = expanded;
@@ -17,15 +20,15 @@ window.onload = function () {
 }
  function expandingAccordions () {
      const accordionHeaders = document.querySelectorAll('.accordion-heading');
-     const expand = document.getElementsByClassName('.expand-all-accordion .expand');
+
 
      Array.prototype.forEach.call(accordionHeaders, accordionHeader => {
-         console.log(accordionHeader);
+
          let target = accordionHeader.nextElementSibling;
          let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
          accordionHeader.setAttribute('aria-expanded', !expanded);
          target.hidden = expanded;
-         // accordionHeader.setAttribute('aria-expanded', 'true');
+
 
      });
 
